e206d9940d9dc1be1e6c08a043105d4316cf3328
Karabo-Motla <161017684+Karabo-Motla@users.noreply.github.com>
2024-11-11 20:25:14 +0200

    Delete READ directory

--- READ/main.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from student import Student
-from admin import Admin
-from story import Story
-from readingSession import ReadingSession
-from audio import Audio
-from readingMaterial import ReadingMaterial 
-
